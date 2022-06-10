f707041e29b141055cd2e966af4ba88e2f79b5e1
Ashish <aashish7322@gmail.com>
2022-06-10 11:33:16 +0530

    ADDED solution of UC2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,18 +7,23 @@ namespace ConsoleApp2
         static void Main(string[] args)
 
         {
+            int WagePerHour = 20;
+            int DayHour = 0;
             Random r = new Random();
             int employee_check = r.Next(0, 2);
 
             if (employee_check == 1)
             {
                 Console.WriteLine("Employee is present");
+                 DayHour = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
 
             }
+            int DailyTotalWage = WagePerHour * DayHour;
+            Console.WriteLine(DailyTotalWage);
 
         }
 
