e4a2f5c575c0370e5894117304903fabdb1a2aa2
Ashish <aashish7322@gmail.com>
2022-06-10 11:39:22 +0530

    Added Solution of UC3

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,12 +10,18 @@ namespace ConsoleApp2
             int WagePerHour = 20;
             int DayHour = 0;
             Random r = new Random();
-            int employee_check = r.Next(0, 2);
+            int employee_check = r.Next(0, 3);
 
             if (employee_check == 1)
             {
                 Console.WriteLine("Employee is present");
                  DayHour = 8;
+            }
+
+            else if(employee_check == 2)
+            {
+                Console.WriteLine("Employee is present and Worked part time");
+                DayHour = 4;
             }
             else
             {
