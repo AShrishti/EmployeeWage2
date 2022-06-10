8108c5b633bfda9ac653fe79d5393d2c31f5091e
Ashish <aashish7322@gmail.com>
2022-06-10 11:17:53 +0530

    solution of UC1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,8 +7,20 @@ namespace ConsoleApp2
         static void Main(string[] args)
 
         {
-            
-            Console.WriteLine("Hello Ashish");
+            Random r = new Random();
+            int employee_check = r.Next(0, 2);
+
+            if (employee_check == 1)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+
+            }
+
         }
-    }
+
+     }
 }
