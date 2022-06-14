11d0adb0517f0a01d53ab4e8efdc8fd8e9c3b386
Ashish <aashish7322@gmail.com>
2022-06-14 20:19:29 +0530

    corrected

--- ConsoleApp2/EmployeeWage.cs
+++ ConsoleApp2/EmployeeWage.cs
@@ -8,12 +8,20 @@ namespace ConsoleApp2
 {
     internal class EmployeeWage
     {
+<<<<<<< HEAD
 
+=======
+        /*
+>>>>>>> parent of 045107b (added with error)
         static void Main(string[] args)
         {
         Program.ComputeEmployeeWage("Reliance",20,23);
          Program.ComputeEmployeeWage("VMart", 25, 25);
 
+<<<<<<< HEAD
         }
+=======
+        }*/
+>>>>>>> parent of 045107b (added with error)
     }
 }
